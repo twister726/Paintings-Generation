66bc39ba661bdbc9669725af69ea1ebe7f940789
riley hadden <rhadden@dsmlp-login.ucsd.edu>
2020-03-13 12:27:48 -0700

    current state

--- genZero.py
+++ genZero.py
@@ -10,7 +10,7 @@ class BaseGenerator(nn.Module):
         self.batch_size = batch_size
         #self.hidden_dim = hidden_dim
         self.device = torch.device("cuda:0")
-        self.embedding_dim = 256
+        self.embedding_dim = embedding_dim
 #        self.initial_fc = nn.Linear(encoded_feature_dim, embedding_dim)
         
         # Embed input vector into tensor
@@ -37,6 +37,8 @@ class BaseGenerator(nn.Module):
             nn.Conv2d(in_channels = 3, out_channels= 3, kernel_size = 3, stride = 1,padding=1),
         )
         
+        self.D0_compress = nn.Linear(self.embedding_dim, 128)
+        
         self.D0_down = nn.Sequential(
             nn.Conv2d(in_channels = 3, out_channels= 64, kernel_size = 4, stride = 2,padding=1),
             #No batch norm/leaky Relu on first
@@ -54,47 +56,32 @@ class BaseGenerator(nn.Module):
             nn.LeakyReLU(inplace=True),
         )
         
-        self.D0_join = nn.Conv2d(in_channels = 768, out_channels = 50, kernel_size=1,stride=1)
+        self.D0_join = nn.Conv2d(in_channels = 640, out_channels = 50, kernel_size=1,stride=1)
         self.D0_judge = nn.Sequential(
             nn.Linear(800, 1),
-            nn.Softmax(),
+            #nn.Softmax(),
         )
 
-
+    def setDiscriminatorGrad(self, grad_on):
+        discParameters = list(self.D0_down.parameters())
+        discParameters.extend(self.D0_compress.parameters())
+        discParameters.extend(self.D0_join.parameters())
+        discParameters.extend(self.D0_judge.parameters())
+        for param in discParameters:
+            param.requires_grad = grad_on
                    
     def forward(self, sentence, imgs):
 
-#         embeds = self.embedding(sentence)
-        
-        #lstm_inp = torch.cat((features.unsqueeze(1), embeds), 1)
 
-        #packed_sequence = pack_padded_sequence(lstm_inp, lengths, batch_first=True)
-        #packed_sequence = pack_padded_sequence(lstm_inp, lengths, batch_first=True, enforce_sorted=False)
-        
-        
-        #packed_out, self.hidden = self.lstm(packed_sequence, self.hidden)
-#         outputs = []
-#         lstmOut, self.hidden = self.lstm(features.unsqueeze(1), self.hidden)
-#         outputs.append(lstmOut)
-#         for i in range(embeds.shape[1] - 1):
-#             lstmOut, self.hidden = self.lstm(embeds[:,i,:].unsqueeze(1), self.hidden)
-#             outputs.append(lstmOut)
-#        print("outputs: {}".format(outputs[0].shape))
-#        print("Ouputs stacked: {}".format(torch.stack(outputs, 1).shape))
-#         fc_out = self.fc(torch.stack(outputs, 1).squeeze(2))
-        
-        #print("FC out; {}".format(fc_out.shape))
-#         _, indices = fc_out.max(2)
-        #word_scores = fc_out
-        #return indices.type(torch.int64).cuda()
         resized = self.F0_FC(sentence)
-        h0 = self.F0(resized.view(self.batch_size, 3, 8, 8))
+        h0 = self.F0(resized.view(imgs.shape[0], 3, 8, 8))
         gen_img = self.G0(h0)
         d_down = self.D0_down(gen_img)
+        d_comp = self.D0_compress(sentence)
         
         #Reshape to concat and join
-        stack = torch.stack([sentence.clone().squeeze(0), sentence.clone().squeeze(0),
-                             sentence.clone().squeeze(0), sentence.clone().squeeze(0)], dim=2)
+        stack = torch.stack([d_comp.clone().squeeze(0), d_comp.clone().squeeze(0),
+                             d_comp.clone().squeeze(0), d_comp.clone().squeeze(0)], dim=2)
         stacked = torch.stack([stack.clone(), stack.clone(), stack.clone(), stack.clone()], dim=3)
         
         d_down_real = self.D0_down(imgs)
@@ -106,50 +93,4 @@ class BaseGenerator(nn.Module):
         
         return gen_img, h0, self.D0_judge(joined_gen), self.D0_judge(joined_real)
     
-#     def generate_caption(self, features, maxSeqLen, temperature, stochastic=False):
-#         # TODO - function for generating caption without using teacher forcing (using network outputs)
-        
-# #        features = self.initial_fc(features)
-# #         print('features shape: ', features.size())
-# #         print('maxseqlen: ', maxSeqLen)
-#         lstm_inp = features.unsqueeze(1)
-#         word_ids = []
-#         self.resetHidden(1)
-#         if stochastic:
-#             for i in range(maxSeqLen):
-#                 lstm_out, _ = self.lstm(lstm_inp)
-#                 fc_out = self.fc(lstm_out.squeeze(1))
-#                 scores = F.softmax(fc_out, dim=1) / temperature
-#                 indices = (torch.distributions.Categorical(scores)).sample()
-#                 word_ids.append(indices)
-#                 lstm_inp = self.embedding(indices).unsqueeze(1)
-                
-                
-#         else:
-#             for i in range(maxSeqLen):
-#                 if i == 0:
-#                     lstm_out, hidden = self.lstm(lstm_inp)
-#                 else:
-# #                     print("Hidden shape")
-# #                     print(hidden[0].shape)
-# #                     print(hidden[1].shape)
-#                     lstm_out, hidden = self.lstm(lstm_inp, hidden)
-                    
-# #                 print("Output size")
-# #                 print(lstm_out.data.shape)
-#                 fc_out = self.fc(lstm_out.squeeze(1))
-#                 _, indices = fc_out.max(1)
 
-                
-#                 lstm_inp = self.embedding(indices).unsqueeze(1)
-# #                 print("Input shape: ")
-# #                 print(lstm_inp.shape)
-#                 word_ids.append(indices.cpu())
-                
-            
-#         word_ids = torch.stack(word_ids,1)
-# #         print('word ids shape: ', word_ids.size())
-#         #        print('word ids shape: ', word_ids.size())
-# #        print('word ids: ', word_ids)
-        
-#        return word_ids
